ba83946e45b1b0e9cc215d6f1b178eabe48dc6e5
Богдан <chappiemeow@gmail.com>
2018-03-13 18:48:42 +0200

    commit

--- src/main/java/com/store/entity/Color.java
+++ src/main/java/com/store/entity/Color.java
@@ -1,14 +1,10 @@
 package com.store.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Color extends AbstractEntity {
     private String cName;
-
 
     public Color() {
     }
